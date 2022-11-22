f4f4682763d74e169ed1e48a996ae5d88d58f99b
umbrellaplug <107957815+umbrellaplug@users.noreply.github.com>
2022-11-22 11:06:29 -0600

    library update

--- matrix/plugin.video.umbrella/resources/lib/modules/player.py
+++ matrix/plugin.video.umbrella/resources/lib/modules/player.py
@@ -99,12 +99,19 @@ class Player(xbmc.Player):
 			item.setInfo(type='video', infoLabels=control.metadataClean(meta))
 			item.setProperty('IsPlayable', 'true')
 			if int(control.playlist.size()) < 1 and self.media_type == 'episode' and self.enable_playnext: #this is the change made for play next from widget.
-				episodelabel = '%sx%02d %s' % (int(season), int(episode), self.meta.get('title'))
-				item.setLabel(episodelabel) #set the episode name here.
-				control.playlist.clear()
-				control.playlist.add(url, item)
-				playerWindow.setProperty('umbrella.playlistStart_position', str(0))
-				control.player.play(control.playlist)
+				try:
+					episodelabel = '%sx%02d %s' % (int(season), int(episode), self.meta.get('title'))
+					if self.meta.get('title'):
+						item.setLabel(episodelabel) #set the episode name here.
+						control.playlist.add(url, item)
+						playerWindow.setProperty('umbrella.playlistStart_position', str(0))
+						control.player.play(control.playlist)
+					else:
+						if debridPackCall: control.player.play(url, item) # seems this is only way browseDebrid pack files will play and have meta marked as watched
+						else: control.resolve(int(argv[1]), True, item)
+				except:
+					if debridPackCall: control.player.play(url, item) # seems this is only way browseDebrid pack files will play and have meta marked as watched
+					else: control.resolve(int(argv[1]), True, item)
 			elif debridPackCall: control.player.play(url, item) # seems this is only way browseDebrid pack files will play and have meta marked as watched
 			else: control.resolve(int(argv[1]), True, item)
 			homeWindow.setProperty('script.trakt.ids', jsdumps(self.ids))
