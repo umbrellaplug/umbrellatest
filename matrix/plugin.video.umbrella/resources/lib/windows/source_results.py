db95192939789689e622f12ba891f7b1592b774d
umbrellaplug <107957815+umbrellaplug@users.noreply.github.com>
2023-04-11 10:53:20 -0500

    appletv 4k fix

--- matrix/plugin.video.umbrella/resources/lib/windows/source_results.py
+++ matrix/plugin.video.umbrella/resources/lib/windows/source_results.py
@@ -161,8 +161,8 @@ class SourceResultsXML(BaseDialog):
 			log_utils.error()
 
 	def get_quality_iconPath(self, quality):
-		from resources.lib.modules import log_utils
-		log_utils.log(' [ plugin.video.umbrella ] quality passed: %s' % str(quality),1)
+		if str(quality) == '4K':
+			quality = '4k'
 		try:
 			return joinPath(transPath('special://home/addons/plugin.video.umbrella/resources/skins/Default/media/resolution'), '%s.png' % quality)
 		except:
@@ -170,6 +170,8 @@ class SourceResultsXML(BaseDialog):
 			log_utils.error()
 
 	def get_provider1_iconPath(self, provider):
+		if str(quality) == '4K':
+			quality = '4k'
 		try:
 			if provider == 'premiumize.me': provider = 'premiumize'
 			return joinPath(transPath('special://home/addons/plugin.video.umbrella/resources/skins/Default/media/resolution1'), '%s.png' % provider)
