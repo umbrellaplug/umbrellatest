1ca3a205151a65f9360b1d863792049019ff0e08
umbrellaplug <107957815+umbrellaplug@users.noreply.github.com>
2023-06-26 09:48:48 -0500

    logging

--- nexus/plugin.video.umbrella/resources/lib/modules/player.py
+++ nexus/plugin.video.umbrella/resources/lib/modules/player.py
@@ -465,6 +465,7 @@ class Player(xbmc.Player):
 
 	def isPlayback(self):
 		# Kodi often starts playback where isPlaying() is true and isPlayingVideo() is false, since the video loading is still in progress, whereas the play is already started.
+		log_utils.log('isPlayBack Call isPlaying: %s isPlayingVideo: %s self.getTime: %s' % (self.isPlaying(), self.isPlayingVideo(), self.getTime()))
 		return self.isPlaying() and self.isPlayingVideo() and self.getTime() >= 0
 
 	def libForPlayback(self):
